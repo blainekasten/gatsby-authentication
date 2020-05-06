7c98b6c7e6b5c27f7d0ad3be6f459a05d22de585
Blaine Kasten <blainekasten@gmail.com>
2020-05-06 14:59:30 -0500

    fix stuff

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,21 @@
+// @ts-ignore
+let localStorage = global.localStorage
+
+if (!localStorage) {
+  const store = {}
+  localStorage = {
+    getItem(key) {
+      return store[key]
+    },
+    setItem(key, value) {
+      store[key] = JSON.stringify(value)
+    },
+    removeItem(key) {
+      delete store[key]
+    },
+  }
+}
+
 export const isLoggedIn = () => localStorage.getItem("LOGIN") === "true"
 
 export const login = () => localStorage.setItem("LOGIN", "true")
--- src/hooks/use-auth-route.ts
+++ src/hooks/use-auth-route.ts
@@ -1,10 +1,13 @@
 import { useNavigate } from "@reach/router"
 import { isLoggedIn } from "../auth"
+import { useEffect } from "react"
 
 export const useAuthRoute = () => {
   const navigate = useNavigate()
 
-  if (isLoggedIn() === false) {
-    navigate("/", { replace: true })
-  }
+  useEffect(() => {
+    if (isLoggedIn() === false) {
+      navigate("/", { replace: true })
+    }
+  }, [])
 }
